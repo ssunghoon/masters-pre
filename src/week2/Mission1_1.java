ea50b1b41ae96617b39e74ace9205ab7c55178a3
ssunghoon <dtg0209@naver.com>
2022-11-08 00:33:20 +0900

    2주차 미션#1 1번문제

---
+++ src/week2/Mission1_1.java
@@ -0,0 +1,16 @@
+package week2;
+
+public class Mission1_1 {
+    public static void main(String[] args) {
+        char[] alpabet = new char[26];
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<alpabet.length; i++) {
+
+            alpabet[i] = (char) ('A' + i);
+
+            sb.append(alpabet[i] + " ");
+        }
+        System.out.println(sb);
+    }
+}
