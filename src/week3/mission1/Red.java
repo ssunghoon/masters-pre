9b0f55ca05d984bf1496fe6729545cbebb81689b
ssunghoon <dtg0209@naver.com>
2022-11-15 19:02:23 +0900

    3주차 미션#1 1번문제

--- src/week3/mission1/Red.java
+++ src/week3/mission1/Red.java
@@ -3,6 +3,7 @@ package week3.mission1;
 public class Red extends Member {
     public Red(String memberName, int price, int stayTime) {
         super(memberName, price, stayTime);
+        discountRate = 0;
         bonusRate = 0.01;
     }
 
